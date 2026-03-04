ab3f49b3bc334a605ae3218bbabf91fa44bf4dcf
TE24B-CENGIZ-DENIZ <deniz.cengiz@elev.ga.ntig.se>
2026-03-04 09:36:12 +0100

    erddsfde

--- Assets/movecontroller.cs
+++ Assets/movecontroller.cs
@@ -1,9 +1,11 @@
+using System.Diagnostics;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class movecontroller : MonoBehaviour
 {
-//  double jmp
+    //  double jmp
     [SerializeField]
     float walkspeed = 2f;
 
@@ -11,51 +13,68 @@ public class movecontroller : MonoBehaviour
     float gravmult = 2;
 
     [SerializeField]
-    float jmpforce=200;
+    float jmpforce = 200;
     Vector2 moveinpt;
     CharacterController cont;
 
-    float velY= 0;
+    float velY = 0;
 
     void Start()
     {
-         cont= GetComponent<CharacterController>();
+        cont = GetComponent<CharacterController>();
     }
 
     void Update()
     {
         // grav
-        velY += Physics.gravity.y*Time.deltaTime * gravmult;
-        if (cont.isGrounded && velY< 0)
+        velY += Physics.gravity.y * Time.deltaTime * gravmult;
+        if (cont.isGrounded && velY < 0)
         {
-            velY= -1;
+            velY = -1;
         }
 
         // move
 
-        Vector3 movement = transform.forward* moveinpt.y +transform.right * moveinpt.x;
+        Vector3 movement = transform.forward * moveinpt.y + transform.right * moveinpt.x;
 
         movement *= walkspeed; // * x y z
 
-        movement.y= velY;
+        movement.y = velY;
 
-        cont.Move(movement* Time.deltaTime);
+        cont.Move(movement * Time.deltaTime);
 
     }
 
     void OnMove(InputValue val)
 
     {
-        moveinpt= val.Get<Vector2>();
+        moveinpt = val.Get<Vector2>();
     }
 
     void OnJump(InputValue val)
     {
+        bool hashopped= false;
+
         if (cont.isGrounded)
         {
-            velY= jmpforce;
+            velY = jmpforce;
         }
-        
+        else
+        {
+            hashopped=true;
+            if(hashopped == true)
+            {
+                velY=jmpforce;
+
+                hashopped=false;
+            }
+            else
+            {
+                velY=0;
+            }
+        }
+       
+
     }
 }
 
