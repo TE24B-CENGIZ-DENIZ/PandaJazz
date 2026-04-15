0e5a188ac3fd7e45417b1b12b78585c6cac36c94
TE24B-CENGIZ-DENIZ <deniz.cengiz@elev.ga.ntig.se>
2026-04-15 09:45:05 +0200

    enemyfollow

--- Assets/EnemyController.cs
+++ Assets/EnemyController.cs
@@ -1,31 +1,24 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    [SerializeField]
+  
     GameObject target;
-    
+
     [SerializeField]
     float EnemySpeed = 5;
 
-    // Update is called once per frame
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player");
+    }
+
+   
     void Update()
     {
-        
-        if (target.transform.position.x < transform.position.x)
-        {
 
-            Vector3 movement = Vector3.left * EnemySpeed * Time.deltaTime;
-            transform.Translate(movement);
-        
-        }
-        else
-        {
-            Vector3 movement = Vector3.right * EnemySpeed * Time.deltaTime; 
-            transform.Translate(movement);
-                                                                                                    // dog water code lmao
-
-        }
-
+       transform.position = Vector3.MoveTowards(transform.position, target.transform.position, EnemySpeed);
     }
+
 }
