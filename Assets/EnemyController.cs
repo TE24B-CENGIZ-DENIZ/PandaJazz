abcaefcd7aef6a20652a2e72a5ec22fc5ddd8e43
TE24B-CENGIZ-DENIZ <deniz.cengiz@elev.ga.ntig.se>
2026-04-28 16:15:16 +0200

    no phuking progress

--- Assets/EnemyController.cs
+++ Assets/EnemyController.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-  
+
     GameObject target;
 
-    float TimeToWait= 2;
-    float TimeToFire = 0 ;
+    float TimeToWait = 2;
+    float TimeToFire = 0;
 
     [SerializeField]
     float EnemySpeed = 5;
@@ -16,30 +16,37 @@ public class EnemyController : MonoBehaviour
     [SerializeField]
     GameObject bulletprefab;
 
-   
+
 
     void Start()
     {
-       target = GameObject.FindGameObjectWithTag("Player");
-       
+        target = GameObject.FindGameObjectWithTag("Player");
+
     }
 
-    
+
 
     void Update()
     {
 
-       transform.position = Vector3.MoveTowards(transform.position, target.transform.position, EnemySpeed);
-       TimeToFire += Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target.transform.position, EnemySpeed);
+        TimeToFire += Time.deltaTime;
 
+
+
+    }
+
+    void FixedUpdate()
+    {
         if (TimeToFire > TimeToWait)
         {
-           
+            
+            
+            
             Instantiate(bulletprefab, transform.position, Quaternion.identity);
             TimeToFire = 0;
         }
-        
-       
+
     }
 
 }
